bb4faea266ab1ff449f3240244683c0f2e7105d3
Jay-Sparks <jay_sparks@icloud.com>
2024-01-01 21:11:24 +0000

    react three fibre lesson from Bruno Simon's threejs journey

---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.js'
+import * as THREE from 'three'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <Canvas
+        // dpr={ [ 1, 2 ] }
+        // flat
+        gl={ {
+            antialias: true,
+            toneMapping: THREE.ACESFilmicToneMapping,
+            outputColorSpace: THREE.SRGBColorSpace
+        } }
+        camera={ {
+            fov: 70,
+            near: 0.1,
+            far: 200,
+            position: [ 3, 2, 6 ]
+        } }
+    >
+
+
+        <Experience />
+    </Canvas>
+)
